112d88cf515f53c2a142995eeac6e58c2c9fceb8
Derik Fischer <dfischer@daitangroup.com>
2017-07-27 15:02:37 -0300

    spring boot test

---
+++ src/main/java/com/daitan/example/daitanExample/DaitanExampleApplication.java
@@ -0,0 +1,12 @@
+package com.daitan.example.daitanExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaitanExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaitanExampleApplication.class, args);
+	}
+}
